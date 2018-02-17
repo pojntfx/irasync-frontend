f85057546876310898e157821bc3071096e48898
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-02-18 00:53:27 +0100

    Added experimental router implementation

---
+++ src/app/content/content-person/content-person.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-person',
+  templateUrl: './content-person.component.html',
+  styleUrls: ['./content-person.component.scss']
+})
+export class ContentPersonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
