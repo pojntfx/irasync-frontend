474f0bf7d9c05013292b7b260e6bf6477d62a541
Felix Pojtinger <felix@pojtinger.com>
2018-02-25 12:59:41 +0100

    Added list component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { render } from "react-dom";
-import "./index.css";
-import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+
+// Semantic UI styles (common)
+import "semantic-ui-css/semantic.min.css";
 
 // Apollo client
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
-render(<App />, document.getElementById("root"));
+// React router
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// Routes
+import Feed from "./routes/Feed";
+import Login from "./routes/Login";
+
+// Components
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+
+render(
+  <Router>
+    <div>
+      <Route path="/" component={Navigation} />
+      <Route exact path="/" component={Feed} />
+      <Route exact path="/login" component={Login} />
+      <Route path="/" component={Footer} />
+    </div>
+  </Router>,
+  document.getElementById("root")
+);
+
+// Enable PWA support
 registerServiceWorker();
