9b4a88729a9154c01cf5db5be1a29ca6ab855520
Felix Pojtinger <felix@pojtinger.com>
2018-03-01 22:31:18 +0100

    Centered signin form and added logos

--- src/components/login/SigninForm.js
+++ src/components/login/SigninForm.js
@@ -3,7 +3,36 @@ import React, { Component } from "react";
 // Router Link
 import { Link } from "react-router-dom";
 
-import { Form, Button } from "semantic-ui-react";
+// Semantic
+import { Form, Button, Image } from "semantic-ui-react";
+
+// Logo
+import logo from "../../assets/logo-light-gradient-green-blue.svg";
+
+// Styled components
+import styled from "styled-components";
+
+// Logo
+const LogoTemplate = ({ className }) => {
+  return (
+    <div>
+      <Image src={logo} fluid centered className={className} />{" "}
+      <div className="text-below-logo">
+        The social network for democratic communities
+      </div>
+    </div>
+  );
+};
+const Logo = styled(LogoTemplate)`
+  /* Show that the heading is not a label */
+  ~ .text-below-logo {
+    margin-bottom: 1rem;
+  }
+  /* Make the logo not that big */
+  max-width: 250px !important;
+  /* Add a margin between the image and the text below it */
+  margin-bottom: 1rem;
+`;
 
 export default class SigninForm extends Component {
   state = { email: "", password: "" };
@@ -30,33 +59,32 @@ export default class SigninForm extends Component {
     const { email, password } = this.state;
 
     return (
-      <div>
-        <Form onSubmit={onSubmit} error>
-          <Form.Group widths="equal">
-            <Form.Input
-              placeholder="Email"
-              name="email"
-              type="email"
-              value={email}
-              onChange={handleInput}
-              required
-              autoFocus
-            />
-            <Form.Input
-              placeholder="Password"
-              name="password"
-              type="password"
-              value={password}
-              onChange={handleInput}
-              required
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Button basic primary content="Sign in" />
-            <Button basic secondary as={Link} to="/signup" content="Sign up" />
-          </Form.Group>
-        </Form>
-      </div>
+      <Form onSubmit={onSubmit} error>
+        <Logo />
+        <Form.Input
+          placeholder="Email"
+          name="email"
+          type="email"
+          value={email}
+          onChange={handleInput}
+          required
+          autoFocus
+        />
+        <Form.Input
+          placeholder="Password"
+          name="password"
+          type="password"
+          value={password}
+          onChange={handleInput}
+          required
+        />
+        <Form.Group widths="equal">
+          <Form.Field>
+            <Button fluid secondary as={Link} to="/signup" content="Sign up" />
+          </Form.Field>
+          <Form.Button fluid primary content="Sign in" />
+        </Form.Group>
+      </Form>
     );
   }
 }
