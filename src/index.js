b76b4698f8da524a4fae623e1ef109274585e3b8
Felix Pojtinger <felix@pojtinger.com>
2018-03-03 19:00:31 +0100

    Extracted index from main file

--- src/index.js
+++ src/index.js
@@ -12,25 +12,8 @@ import { createHttpLink } from "apollo-link-http";
 import { setContext } from "apollo-link-context";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
-// React router
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
-
-// Routes/Screens
-import Home from "./routes/Home";
-import Signin from "./routes/Signin";
-import { Signup } from "./routes/Signup";
-import { Private as PrivateRoute } from "./routes/Private";
-import FourZeroFour from "./routes/404";
-
-// Update authorization state
-const authorizationState = () => {
-  return localStorage.getItem("token") ? true : false;
-};
+// Router
+import Index from "./routes/index";
 
 // Authorization header (token will be pulled from localStorage every time a request is sent)
 const authLink = setContext((_, { headers }) => {
@@ -58,61 +41,7 @@ const client = new ApolloClient({
 
 render(
   <ApolloProvider client={client}>
-    <Router>
-      <Switch>
-        {/* Home */}
-        <Route
-          exact
-          path="/"
-          render={props => (
-            <Home isAuthenticated={() => authorizationState()} {...props} />
-          )}
-        />
-        <Route exact path="/feed" render={() => <Redirect to="/" />} />
-        <Route exact path="/f" render={() => <Redirect to="/" />} />
-
-        {/* Drafts */}
-        <PrivateRoute
-          path="/drafts"
-          isAuthenticated={authorizationState}
-          render={props => (
-            <Home isAuthenticated={() => authorizationState()} {...props} />
-          )}
-        />
-        <PrivateRoute
-          path="/d"
-          isAuthenticated={authorizationState}
-          render={() => <Redirect to="/drafts" />}
-        />
-
-        {/* Signup */}
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/su" render={() => <Redirect to="/signup" />} />
-
-        {/* Signin */}
-        <Route
-          exact
-          path="/signin"
-          render={props => (
-            <Signin
-              isAuthenticated={() => authorizationState()}
-              onSuccessfullSignin={() => authorizationState()}
-              {...props}
-            />
-          )}
-        />
-        <Route exact path="/si" render={() => <Redirect to="/signin" />} />
-
-        {/* 404 */}
-        <Route component={FourZeroFour} />
-
-        {/* <Route path="/c" component={Community} />
-        <Route path="/community" component={Community} />
-
-        <Route path="/p" component={Person} />
-        <Route path="/community" component={Person} /> */}
-      </Switch>
-    </Router>
+    <Index />
   </ApolloProvider>,
   document.getElementById("isf")
 );
