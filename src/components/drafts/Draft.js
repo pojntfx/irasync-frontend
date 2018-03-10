e8ec121ff6533b4daa036e306d8fcd8d570c3fb5
Felix Pojtinger <felix@pojtinger.com>
2018-03-10 16:12:17 +0100

    Added draft deletion confirmation dialog

--- src/components/drafts/Draft.js
+++ src/components/drafts/Draft.js
@@ -12,6 +12,7 @@ import styled from "styled-components";
 // Components
 import MarkdownDisplay from "../global/MarkdownDisplay";
 import PublishDialog from "./PublishDialog";
+import DeleteDialog from "./DeleteDialog";
 
 const CardActionGroup = styled(Button.Group)`
   overflow-x: auto;
@@ -21,7 +22,8 @@ const CardActionGroup = styled(Button.Group)`
 
 export default class Draft extends Component {
   state = {
-    publishModalIsActive: false
+    publishModalIsActive: false,
+    deleteModalIsActive: false
   };
 
   onPublishDialog = () => {
@@ -39,9 +41,15 @@ export default class Draft extends Component {
   };
 
   onCancel = () => {
-    this.setState({
-      publishModalIsActive: false
-    });
+    // Since this is being used with both dialogs, this will determine which dialog
+    // has emitted an onCancel event.
+    this.state.publishModalIsActive
+      ? this.setState({
+          publishModalIsActive: false
+        })
+      : this.setState({
+          deleteModalIsActive: false
+        });
   };
 
   onEdit = () => {
@@ -49,21 +57,37 @@ export default class Draft extends Component {
     console.log(`Editing draft ${id}!`);
   };
 
+  onDeleteDialog = () => {
+    this.setState({
+      deleteModalIsActive: true
+    });
+  };
+
   onDelete = () => {
+    this.setState({
+      deleteModalIsActive: false
+    });
     const { id } = this.props;
     this.props.onDelete(id);
   };
 
   render() {
     const { title, text, createdAt } = this.props;
-    const { onPublishDialog, onPublish, onCancel, onEdit, onDelete } = this;
-    const { publishModalIsActive } = this.state;
+    const {
+      onPublishDialog,
+      onPublish,
+      onCancel,
+      onEdit,
+      onDeleteDialog,
+      onDelete
+    } = this;
+    const { publishModalIsActive, deleteModalIsActive } = this.state;
 
     return (
       <Card fluid>
         <Card.Content>
           <Card.Header>{title}</Card.Header>
-          <Card.Meta>Created {postAge(createdAt)} ago</Card.Meta>
+          <Card.Meta>{postAge(createdAt)} ago</Card.Meta>
           <Card.Description>
             <MarkdownDisplay content={text} />
           </Card.Description>
@@ -81,7 +105,12 @@ export default class Draft extends Component {
             <Button disabled basic color="blue" onClick={onEdit} size="small">
               <Icon name="edit" />Edit
             </Button>
-            <Button basic color="red" onClick={onDelete} size="small">
+            <DeleteDialog
+              onDelete={onDelete}
+              onCancel={onCancel}
+              active={deleteModalIsActive}
+            />
+            <Button basic color="red" onClick={onDeleteDialog} size="small">
               <Icon name="trash" />Delete
             </Button>
           </CardActionGroup>
