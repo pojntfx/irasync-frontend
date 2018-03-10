a05da7fabbbf4d31b470b2947df74569f949803d
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-03-10 15:54:18 +0100

    Added publish confirmation dialog

---
+++ src/components/drafts/PublishDialog.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+// Semantic
+import { Modal, Button } from "semantic-ui-react";
+
+// Styled components
+import styled from "styled-components";
+
+class PublishDialog extends Component {
+  onClose = () => {
+    this.props.onCancel();
+  };
+
+  onPublish = () => {
+    this.props.onPublish();
+  };
+
+  render() {
+    const { onClose, onPublish } = this;
+
+    const { className, active } = this.props;
+
+    return (
+      <Modal
+        basic
+        dimmer="inverted"
+        open={active}
+        onClose={onClose}
+        className={className}
+        size="small"
+      >
+        <Modal.Header>
+          Are you sure you want to publish this draft?
+        </Modal.Header>
+        <Modal.Content>
+          Once you{"'"}ve published this draft, it will be visible to the whole
+          world.
+        </Modal.Content>
+        <Modal.Actions>
+          <Button
+            secondary
+            icon="cancel"
+            labelPosition="left"
+            content="No, don't publish this draft"
+            onClick={onClose}
+          />
+          <Button
+            primary
+            icon="send"
+            labelPosition="left"
+            content="Yes, publish this draft"
+            onClick={onPublish}
+          />
+        </Modal.Actions>
+      </Modal>
+    );
+  }
+}
+
+// Center the modal vertically
+export default styled(PublishDialog)`
+  margin: 0 auto !important;
+  margin-top: 30vh !important;
+`;
