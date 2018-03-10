ebda834d1349a53980725576f9418c2ada819222
Felix Pojtinger <felix@pojtinger.com>
2018-03-10 15:54:18 +0100

    Added publish confirmation dialog

--- src/components/drafts/Draft.js
+++ src/components/drafts/Draft.js
@@ -11,6 +11,7 @@ import styled from "styled-components";
 
 // Components
 import MarkdownDisplay from "../global/MarkdownDisplay";
+import PublishDialog from "./PublishDialog";
 
 const CardActionGroup = styled(Button.Group)`
   overflow-x: auto;
@@ -19,9 +20,28 @@ const CardActionGroup = styled(Button.Group)`
 `;
 
 export default class Draft extends Component {
+  state = {
+    publishModalIsActive: false
+  };
+
+  onPublishDialog = () => {
+    this.setState({
+      publishModalIsActive: true
+    });
+  };
+
   onPublish = () => {
+    this.setState({
+      publishModalIsActive: false
+    });
     const { id } = this.props;
     this.props.onPublish(id);
+  };
+
+  onCancel = () => {
+    this.setState({
+      publishModalIsActive: false
+    });
   };
 
   onEdit = () => {
@@ -36,7 +56,8 @@ export default class Draft extends Component {
 
   render() {
     const { title, text, createdAt } = this.props;
-    const { onPublish, onEdit, onDelete } = this;
+    const { onPublishDialog, onPublish, onCancel, onEdit, onDelete } = this;
+    const { publishModalIsActive } = this.state;
 
     return (
       <Card fluid>
@@ -49,7 +70,12 @@ export default class Draft extends Component {
         </Card.Content>
         <Card.Content extra>
           <CardActionGroup>
-            <Button basic color="green" onClick={onPublish} size="small">
+            <PublishDialog
+              onPublish={onPublish}
+              onCancel={onCancel}
+              active={publishModalIsActive}
+            />
+            <Button basic color="green" onClick={onPublishDialog} size="small">
               <Icon name="send" />Publish
             </Button>
             <Button disabled basic color="blue" onClick={onEdit} size="small">
